4025de090f6402a7e10caae06efb911e5c6cb357
Dominador G de la Cruz Jr <domenikos858@gmail.com>
2020-03-23 21:25:22 -0700

    updates

--- script.js
+++ script.js
@@ -1,13 +1,110 @@
-// Assignment Code
+//var Ids
 var passwordTextArea = document.getElementById("password");
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var copyButton = document.getElementById("copyButton");
 
-  passwordText.value = password;
+//arrays of objects with information about character sets
+var charsets = [
+ 
+  {
+    name: "lowercaseCharset",
+    characters: "abcdefghijklmnopqrstuvwxyz",
+    id: document.getElementById("checkLowercase")
+  },
+  {
+    name: "uppercaseCharset",
+    characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    id: document.getElementById("checkUppercase")
+  },
+  {
+    name: "numericCharset",
+    characters: "123456789",
+    id: document.getElementById("checkNumbers")
+  },
+  {
+    name: "specialCharset",
+    characters: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+    id: document.getElementById("checkSpecial"),
+  }
+]
 
+
+//returns true if at least one of the checkboxes are checked
+function checked(){
+  for (i=0; i < charsets.length; i++){
+    if(charsets[i].id.checked){
+      return true;
+    }
+  }
 }
 
-// Add event listener to generate button
+//object containing error messages
+var errorMessages={
+  errorLengthCheck: "Password length must be between 8-128 characters and select at least one character type.",
+  errorLength: "Password length must be between 8-128 characters.",
+  errorCheck: "Please select at least one character type."
+}
+
+//gives feedback when parameters are not met
+function errorHandling(){
+  copyButton.setAttribute("disabled","");
+    //error: user needs to adjust number of characters and check at least one checkbox
+  if(!checked() && (charCount < 8 || charCount > 128)){
+      password=errorMessages.errorLengthCheck;
+      passwordTextArea.innerText=password;
+    // error: user needs to adjust number of characters
+  } else if (charCount < 8 || charCount > 128){
+      password="errorMessages.errorLength"
+      passwordTextArea.innerText=errorMessages.errorLength;
+      //error user needs to check at least one checkbox
+  } else if(!checked()){
+      password = errorMessages.errorCheck;
+      passwordTextArea.innerText = password;
+  } else {
+      //no error, so user is able to use copy button to copy password
+      copyButton.removeAttribute("disabled");
+      //return false when no error handling needed
+      return false;
+  }
+}
+
+//called when 'generate password' button is clicked
+function generatePassword() {
+  //no character sets have been chosen yet
+  var passwordCharset = "";
+  //the password resets to blank each time generatePassword is called
+  password="";
+  //when generate password is called, password length is checked
+  charCount=parseInt(document.getElementById('passwordLength').value);
+  if (errorHandling()===false){
+    //adds selected  character sets to pool of available characters to use in password
+    for (i = 0; i<charsets.length; i++){
+      if (charsets[i].id.checked){
+        passwordCharset = passwordCharset + charsets[i].characters;
+      }
+    }
+    //for each character of the length of the selected password length, a random character is selected from the pool of selected characters
+    for (var i = 0; i < charCount; i++) {
+      password = password + passwordCharset[Math.floor((Math.random()* passwordCharset.length))]
+    }
+    //The read only text area is then filled with the generated password
+    passwordTextArea.innerText = password;
+  }
+}
+
+var passwordTextArea = document.getElementById("password");
+var copyButton = document.getElementById("copyButton");
+
+
+//event listeners
 document.getElementById("generatePasswordButton").addEventListener("click", generatePassword);
+document.getElementById("copyButton").addEventListener("click", copyText);
+
+
+
+//This will select and copy text/s from the text area for pasting.
+//Source:https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+function copyText(){
+  passwordTextArea.select();
+  passwordTextArea.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+}
